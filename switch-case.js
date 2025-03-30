bf71fc8a0fdcfd180bf6b7a31f072b001df2da5f
Dmitrii112233 <dmitriiberezovskiy@gmail.com>
2025-03-30 18:11:27 +0300

    add resolved content (with examples of function call)

--- switch-case.js
+++ switch-case.js
@@ -1,1 +1,27 @@
+// Задача 1: Вывод числа словами (Switch Case)
 
+function digitToWord(n) {
+    let result;
+
+    // + Используем switch-case для сопоставления числа и слова
+    switch (n) {
+        case 0: result = "Zero"; break;
+        case 1: result = "One"; break;
+        case 2: result = "Two"; break;
+        case 3: result = "Three"; break;
+        case 4: result = "Four"; break;
+        case 5: result = "Five"; break;
+        case 6: result = "Six"; break;
+        case 7: result = "Seven"; break;
+        case 8: result = "Eight"; break;
+        case 9: result = "Nine"; break;
+        default: result = n + " is not a digit";
+    }
+
+    return result;
+}
+
+// + Примеры использования:
+console.log("== Задача 1 ==");
+console.log(digitToWord(1));   // One
+console.log(digitToWord(10));  // 10 is not a digit
