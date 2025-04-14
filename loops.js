e084f1ae342c9afb586e606a991c4d14e71e291c
Dmitrii112233 <dmitriiberezovskiy@gmail.com>
2025-04-14 10:24:10 +0300

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,1 +1,39 @@
+let a = parseInt(prompt("Enter the starting number (a):"));
+let b = parseInt(prompt("Enter the ending number (b):"));
+
+// a <= b
+if (a > b) {
+    [a, b] = [b, a];
+}
+
+// ----------- FOR loop -----------
+let sumFor = 0;
+for (let i = a; i <= b; i++) {
+    if (i % 2 === 0) {
+        sumFor += i;
+    }
+}
+console.log("Sum using for: " + sumFor);
+
+// ----------- WHILE loop -----------
+let sumWhile = 0;
+let i = a;
+while (i <= b) {
+    if (i % 2 === 0) {
+        sumWhile += i;
+    }
+    i++;
+}
+console.log("Sum using while: " + sumWhile);
+
+// ----------- DO...WHILE loop -----------
+let sumDoWhile = 0;
+i = a;
+do {
+    if (i % 2 === 0) {
+        sumDoWhile += i;
+    }
+    i++;
+} while (i <= b);
+console.log("Sum using do...while: " + sumDoWhile);
 
