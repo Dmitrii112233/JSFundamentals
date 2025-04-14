1ce11774eb1e8c7c891ba1fb34110390bbf7f956
Dmitrii112233 <dmitriiberezovskiy@gmail.com>
2025-04-14 10:26:36 +0300

    Update user_auth.js

--- user_auth.js
+++ user_auth.js
@@ -1,1 +1,20 @@
+// Задача 11: Оптимизированная проверка логина и пароля с динамическим вводом
 
+const correctUsername = "admin";
+const correctPassword = "123456";
+
+const username = prompt("Введите имя пользователя:");
+const password = prompt("Введите пароль:");
+
+//  проверка
+if (!username?.trim() || !password?.trim()) {
+  console.log("Validation Error");
+  
+} else if (username !== correctUsername || password !== correctPassword) {
+  const errorMessage = username !== correctUsername
+    ? "Incorrect username"
+    : "Incorrect password";
+  console.log(errorMessage);
+} else {
+  console.log(`Hello ${username}`);
+}
