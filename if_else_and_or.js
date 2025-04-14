f839de334d531d1023f0cf7525febbdeedf519fc
Dmitrii112233 <dmitriiberezovskiy@gmail.com>
2025-04-14 10:22:04 +0300

    Update if_else_and_or.js

--- if_else_and_or.js
+++ if_else_and_or.js
@@ -1,1 +1,18 @@
 
+let age = 25;
+let start = 18;
+let end = 30;
+
+
+if (!(age >= start && age <= end)) {
+    console.log(`Value ${age} does NOT belong to the segment [${start} ${end}]`);
+} else {
+    console.log(`Value ${age} belongs to the segment [${start} ${end}]`);
+}
+
+
+if (age < start || age > end) {
+    console.log(`Value ${age} does NOT belong to the segment [${start} ${end}]`);
+} else {
+    console.log(`Value ${age} belongs to the segment [${start} ${end}]`);
+}
